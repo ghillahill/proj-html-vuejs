718dc250942a1f6e0f79a5e697d81287522c3720
Alberto <thebertsinfo@gmail.com>
2020-12-01 16:42:24 +0100

    Creata sezione network con pie-chart

--- main.js
+++ main.js
@@ -7,6 +7,13 @@ var app = new Vue({
             'Skills',
             'Partners',
             'Blog'
+        ],
+        sponsorItems: [
+            'img/logo-1.png',
+            'img/logo-2.png',
+            'img/logo-3.png',
+            'img/logo-4.png',
+            'img/logo-5.png'            
         ]
     },
     methods: {
