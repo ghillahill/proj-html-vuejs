a42e869d1ce08faa1c1fc09a6af825cbc400f0ba
Alberto <thebertsinfo@gmail.com>
2020-12-03 10:50:11 +0100

    Creato Footer Up e Down: Liste Menu fatte con struttura dati Vue

--- main.js
+++ main.js
@@ -13,8 +13,34 @@ var app = new Vue({
             'img/logo-2.png',
             'img/logo-3.png',
             'img/logo-4.png',
-            'img/logo-5.png'            
-        ]
+            'img/logo-5.png'
+        ],
+        footerMenu: {
+            about: [
+                'The Company',
+                'Institutional',
+                'Social & Events',
+                'Innovation',
+                'Environment',
+                'Technology',
+            ],
+            transport: [
+                'Industrialized',
+                'Chemicals',
+                'Packaged Liquids',
+                'Construction',
+                'Laminated Wood',
+                'And others',
+            ],
+            support: [
+                'Responsibility',
+                'Terms of Use',
+                'About Cookies',
+                'Privacy Policy',
+                'Accessibility',
+                'Information',
+            ]
+        }
     },
     methods: {
     }
